b0cdfd4605836dcfd9db260a8ed69ccb4068605e
browniu <niuniusoga@qq.com>
2019-10-20 15:14:28 +0800

    更新一些周期函数

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,6 +3,16 @@ import styles from './styles.module.scss'
 import cn from 'classnames'
 
 export default class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+    }
+
+    static defaultProps = {};
+
     render() {
         return (
             <div className={cn(styles.root, 'browniu-app-root')}>
